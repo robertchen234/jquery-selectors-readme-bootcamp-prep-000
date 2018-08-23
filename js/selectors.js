b9df53c9b669e0c522e72c191ea23fc39093839d
Robert Chen <robertchen234@gmail.com>
2018-08-23 19:38:57 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,12 @@
 // declare your functions here...
 function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
   
 }
